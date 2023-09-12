de1be00997c81fabc49d6d8aa99e3021c53d08f0
monica <mounica.maganthi@gmail.com>
2023-09-12 17:27:08 +0530

    adding

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+
+// 1. Question
+
+var numRows = 5;
+
+for (var i = 1; i <= numRows; i++) {
+  var row = '';
+  for (var j = 1; j <= numRows - i; j++) {
+    row += ' '; 
+  }
+  for (var k = 1; k <= 2 * i - 1; k++) {
+    row += '*'; 
+  }
+  console.log(row);
+}
+
