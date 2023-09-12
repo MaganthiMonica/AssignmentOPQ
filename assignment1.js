5ff1e4ad992beaa17c1865dee8448bb006ff2a99
monica <mounica.maganthi@gmail.com>
2023-09-12 20:25:10 +0530

    adding

--- assignment1.js
+++ assignment1.js
@@ -47,5 +47,47 @@ const N = 10;
 const result = findSumOfNumbers(N);
 console.log(`The sum of the first ${N} numbers is: ${result}`);
 
+// 4. diamond shape
+
+var numRows = 5;
+
+for (var i = 1; i <= numRows; i++) {
+  var row = '';
+  var space = '';
+  var stars = '';
+  
+  for (var j = 1; j <= numRows - i; j++) {
+    space += ' ';
+  }
+
+  for (var k = 1; k <= 2 * i - 1; k++) {
+    stars += '*';
+  }
+
+  // Combine spaces and asterisks for the row
+  row = space + stars;
+  
+  console.log(row);
+}
+
+for (var i = numRows - 1; i >= 1; i--) {
+  var row = '';
+  var space = '';
+  var stars = '';
+  
+ 
+  for (var j = 1; j <= numRows - i; j++) {
+    space += ' '
+  }
+  
+  for (var k = 1; k <= 2 * i - 1; k++) {
+    stars += '*';
+  }
+
+  row = space + stars;
+  console.log(row);
+}
+
+
 
 
