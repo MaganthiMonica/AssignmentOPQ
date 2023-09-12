b20df4faf0fde5c67f6261fd49f1df07dc48b5b9
monica <mounica.maganthi@gmail.com>
2023-09-12 19:38:32 +0530

    adding

--- assignment1.js
+++ assignment1.js
@@ -1,5 +1,6 @@
 
-// 1. Question
+// 1.  Generate * pattern to construct a pyramid
+    
 
 var numRows = 5;
 
@@ -15,5 +16,19 @@ for (var i = 1; i <= numRows; i++) {
 }
 
 
-// 2. Question
+// 2. Upside down pyramid
+
+var numRows = 5;
+
+for (var i = numRows; i >= 1; i--) { 
+  var row = '';
+  for (var j = 1; j <= numRows - i; j++) {
+    row += ' '; 
+  }
+  for (var k = 1; k <= 2 * i - 1; k++) {
+    row += '*'; 
+  }
+  console.log(row);
+}
+
 
